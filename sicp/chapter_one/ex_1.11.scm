206bcdd8d4443b20e9e9a6bbd4ad6fe87f043dd1
Pablo Curell <pablocm90@gmail.com>
2019-05-31 08:29:21 +0200

    [1.11] [1.12]

--- sicp/chapter_one/ex_1.11.scm
+++ sicp/chapter_one/ex_1.11.scm
@@ -3,13 +3,16 @@
   number
   ((+ (tree-f (- number 1) (* (tree-f (- number 2)) 2) (* 3 (tree-f (- number 3))))))))
 
-(define (f number)
-  (f-iter 1 2 3 number)
-  (define (f-iter n1 n2 n3 count)
-    (if (< count 3)
-    count
-    (f-iter ()))))
+;(define (f number)
+;  (f-iter 1 2 3 number)
+;  (define (f-iter n1 n2 n3 count)
+;    (if (< count 3)
+;    count
+;    (f-iter ()))))
 
 
 
-(define(fibn)(fib-iter1 0n))(define(fib-iterabcount)(if(=count0)b(fib-iter(+ab)a(-count1))))
+;(define(fibn)(fib-iter1 0n))(define(fib-iterabcount)(if(=count0)b(fib-iter(+ab)a(-count1))))
+
+
+;explanation here: https://codology.net/post/sicp-solution-exercise-1-11/
